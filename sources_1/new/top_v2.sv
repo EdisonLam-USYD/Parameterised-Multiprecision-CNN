c2bad856b6ced07bfbd759d47b44782b1d999033
EdisonLam-USYD <80366397+EdisonLam-USYD@users.noreply.github.com>
2023-10-20 16:51:00 +1100

    desired params

--- sources_1/new/top_v2.sv
+++ sources_1/new/top_v2.sv
@@ -2,16 +2,16 @@
 
 module top_v2 #(
     // convolution + pooling layer(s) top parameters
-    N = 3, BitSize = 32, ImageWidth = 8, PoolingN = 2, 
-    C1NumberOfK = 4, C2NumberOfK = 4, C2ProcessingElements = 2,
-    C1KernelBitSize = 4, C2KernelBitSize = 4,
-    [C1KernelBitSize*(N*N)-1:0] C1kernel [C1NumberOfK-1:0] = {'0,'0,'0,'0},
-    [C2KernelBitSize*(N*N)-1:0] C2kernel [C2NumberOfK-1:0] = {'0,'0,'0,'0},
+    N = 3, BitSize = 8, ImageWidth = 8, PoolingN = 2, 
+    C1NumberOfK = 8, C2NumberOfK = 8, C2ProcessingElements = 2,
+    C1KernelBitSize = 8, C2KernelBitSize = 8,
+    [C1KernelBitSize*(N*N)-1:0] C1kernel [C1NumberOfK-1:0] = {'0,'0,'0,'0,'0,'0,'0,'0},
+    [C2KernelBitSize*(N*N)-1:0] C2kernel [C2NumberOfK-1:0] = {'0,'0,'0,'0,'0,'0,'0,'0},
     // dnn top parameters
     M_W_BitSize = 8, NumLayers = 4, MaxNumNerves = 8,
-    integer LWB [NumLayers-1:0] = '{4, 2, 8, 2}, // left to right 
-    integer LNN [NumLayers-1:0] = '{2, 8, 4, 6}, // left to right
-    integer DNN_Depths [NumLayers-1:0] = '{1, 4, 2, 1},   // left to right
+    integer LWB [NumLayers-1:0] = '{8, 8, 8, 8}, // left to right 
+    integer LNN [NumLayers-1:0] = '{8, 8, 8, 8}, // left to right
+    integer DNN_Depths [NumLayers-1:0] = '{4, 4, 4, 1},   // left to right
     LatencyDelay = 3                        // increase to ensure all output of the FCL is given in one go (required due to no stall signals)
     )
     (
